8daa575d0627c7fef8e3c4a7eb5659ad3ad49dc6
ShadyNagy <info@shadynagy.com>
2021-08-07 20:23:30 +0200

    sample app fixed.

--- sample/app/services/data.service.ts
+++ sample/app/services/data.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { ApiResponse } from '../models/response';
-import { DebouncerHelperService } from './debouncer-helper.service';
+import { DebouncerHelper } from 'projects/ts-sn-utilities';
 
 
 @Injectable({
@@ -10,13 +10,16 @@ import { DebouncerHelperService } from './debouncer-helper.service';
 })
 export class DataService {
 
-  constructor(private http: HttpClient, private debouncerHelperService: DebouncerHelperService) {}
+  debouncerHelper = new DebouncerHelper();
+
+  constructor(private http: HttpClient) {}
 
   private filterBy(value: string): Observable<ApiResponse> {
     return this.http.get<ApiResponse> (`https://api.jikan.moe/v3/search/anime?q=${value}`);
   }
 
   getFilteredByCode(code: string): Observable<ApiResponse> {
-    return this.debouncerHelperService.debouncerOnly<string, ApiResponse>(1000, code, (request) => this.filterBy(request));
+    this.debouncerHelper.resetData();
+    return this.debouncerHelper.debouncerOnly<string, ApiResponse>(1000, code, (request) => this.filterBy(request));
   }
 }
